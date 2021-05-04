c2cff980877addc80479f21d4248ee2d78d9d9f4
xincan <xincan@mogu.com>
2021-05-04 13:00:40 +0800

    feat: v5.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import moment from "moment";
+// import { getUsers } from "./common/usersAPI";
+const getUserModule = () => import(/* webpackChunkName: "usersAPI" */ "./common/usersAPI");
+
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    // getUsers().then(json => console.log(json));
+    getUserModule().then(({ getUsers }) => {
+        getUsers().then(json => console.log(json));
+    });
+});
